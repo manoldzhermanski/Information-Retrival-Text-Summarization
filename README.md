4489eb2edfe80a8deca0cf2c559ea0549e18254f
Manol <moncho.dzhermanski@abv.bg>
2023-12-03 10:39:36 +0200

    Hid the file path of the file; Added Personal Motivation For The Project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Information-Retrival-Text-Summarization
+# Text summarization in Bulgarian/Spanish
+
+#### Created by Manol Dzhermanski
+
+---
+
+# Personal Motivation For The Project
+My family has always been keen on education and teaching.
+As a matter of fact we have 4 teachers:
+* My grandmother - an elementary school teacher (retired)
+* My aunt - a French teacher (retired)
+* My sister - a Spanish and Bulgarian teacher (active)
+* Myself - a Math teacher (avoiding at all costs)
+
+With this project I'm aiming to help out my sister's students
+master reading comprehension by doing a summary on the texts
+they are studying. The project can be used whenever the students
+face difficulties or to verify their understanding.
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import tkinter as tk
+from tkinter import filedialog
 from tkinterdnd2 import TkinterDnD, DND_FILES  # Import necessary libraries
-from tkinter import filedialog  # Import filedialog for browsing files
 import PyPDF2  # For extracting text from PDF files
-import os  # For path handling
+
 
 # Function to extract text from a PDF file
 def extract_text(file_path):
-    if file_path.lower().endswith('.pdf'):  # Check if the file is a PDF
+    # Check if the file is a PDF
+    if file_path.lower().endswith('.pdf'):
         try:
             with open(file_path, 'rb') as pdf_file:
                 pdf_reader = PyPDF2.PdfReader(pdf_file)
@@ -17,48 +18,47 @@ def extract_text(file_path):
                     page = pdf_reader.pages[page_num]
                     pdf_text += page.extract_text()
 
+            # Clear the Text widget and insert extracted text
             extracted_text.delete(1.0, tk.END)
-            extracted_text.insert(tk.END, pdf_text)  # Insert extracted text into the Text widget
+            extracted_text.insert(tk.END, pdf_text)
         except Exception as e:
+            # Handle exceptions while reading or extracting text from the PDF
             extracted_text.delete(1.0, tk.END)
-            extracted_text.insert(tk.END, f"Error: {e}")  # Display error if text extraction fails
+            extracted_text.insert(tk.END, f"Error: {e}")
     else:
+        # Display a message if a non-PDF file is selected/dropped
         extracted_text.delete(1.0, tk.END)
-        extracted_text.insert(tk.END, "Please select/drop a PDF file.")  # Prompt to select a PDF file
+        extracted_text.insert(tk.END, "Please select/drop a PDF file.")
 
-# Function to browse for a file using filedialog
-def browse_file():
-    file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf"), ("All files", "*.*")])
-    if file_path:
-        text_display.delete(1.0, tk.END)
-        text_display.insert(tk.END, os.path.normpath(file_path))  # Display the normalized file path
-        extract_text(file_path)
 
 # Function triggered on drop event
 def on_drop(event):
-    file_path = event.data.strip('{}')  # Extract file path and remove curly braces (if present)
-    text_display.delete(1.0, tk.END)
-    text_display.insert(tk.END, os.path.normpath(file_path))  # Display the normalized file path
+    file_path = event.data.strip('{}')
     extract_text(file_path)
+
+
+# Function to browse and select a file using filedialog
+def browse_file():
+    file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf"), ("All files", "*.*")])
+    if file_path:
+        extract_text(file_path)
+
 
 # Create the main Tkinter window
 root = TkinterDnD.Tk()
 root.title("Text Extractor")  # Set the window title
 
-# Create a Text widget for displaying the dropped or selected file path
-text_display = tk.Text(root, height=1, width=40)
-text_display.pack(padx=20, pady=10)
-
-# Enable drag and drop functionality on the Text widget
-text_display.drop_target_register(DND_FILES)
-text_display.dnd_bind('<<Drop>>', on_drop)
-
-# Create a button to browse for a file
-browse_button = tk.Button(root, text="Browse File", command=browse_file)
-browse_button.pack()
-
-# Create another Text widget for displaying the extracted text
+# Create a Text widget for displaying the extracted text
 extracted_text = tk.Text(root, height=20, width=60)
 extracted_text.pack(padx=20, pady=10)
+extracted_text.insert(tk.END, "Choose your file by clicking the Browse File button or Drag and Drop the file")
+
+# Enable drop target functionality for the Text widget
+extracted_text.drop_target_register(DND_FILES)
+extracted_text.dnd_bind('<<Drop>>', on_drop)
+
+# Create a button for browsing files
+browse_button = tk.Button(root, text="Browse File", command=browse_file)
+browse_button.pack(padx=20, pady=15)  # Place the button in the window
 
 root.mainloop()  # Start the main event loop
